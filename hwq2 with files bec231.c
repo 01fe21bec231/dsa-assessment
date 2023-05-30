774c9b4f20f223e9551ac676577f64dfc3b15e78
Anusha.A.D <97115653+01fe21bec231@users.noreply.github.com>
2023-05-30 20:41:54 +0530

    Update hwq2 with files bec231.c

--- hwq2 with files bec231.c
+++ hwq2 with files bec231.c
@@ -1,99 +1,99 @@
-#include<stdio.h>
-#include<stdlib.h>
-void find(char str[10][100]);
-int length(char str[]);
+#include <stdio.h>
+#include <stdlib.h>
+#define m 10
+#define n 100
+
+int length(const char str[]);
+void findandReplace(char str[][n]);
 
 int main()
 {
-    FILE *s;
- char str[11][100];
- s=fopen("123.txt","r");
- for(int i=0;i<10;i++)
- {
-     fgets(str[i],200,s);
- }
- for(int i=0;i<10;i++)
- {
-     puts(str[i]);
- }
- find(str);
- return 0;
-}
-int length(char str[])
-{
+     FILE *fp1;
+    char str[m][n];
 
-    int i,ll=0;
-    while(str[i]!='\0')
+    fp1 = fopen("123.txt", "r");
+    if (fp1 == NULL)
+    {
+        printf("not found.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        fgets(str[i], n, fp1);
+    }
+
+    fclose(fp1);
+
+    for (int i = 0; i < m; i++)
+    {
+        puts(str[i]);
+    }
+
+    findandReplace(str);
+
+    return 0;
+}
+
+int length(const char str[])
+{
+    int ll = 0;
+    while (str[ll] != '\0')
     {
         ll++;
-        i++;
     }
-
     return ll;
 }
-void find(char str[][100])
+
+void findandReplace(char str[][n])
 {
-    char hold[26],str1[10],str2[26];
-    printf("enter the key \n");
-    gets(hold);
+    char new_key[26];
+    char str2[n];
+    int i, j, k, n;
 
-    int i=0,j=0,k=0,l,n=0;
-   for(int i=0;i<10;i++)
-   { j=0;
-    while(str[i][j]!='\0')
+    printf("Enter the key");
+    fgets(new_key, sizeof(new_key), stdin);
+    new_key[length(new_key) - 1] = '\0'; // Remove newline character
+
+    printf("Enter the string to be replaced: ");
+    fgets(str2, sizeof(str2), stdin);
+    str2[length(str2) - 1] = '\0'; // Remove newline character
+
+    int new_keyLength = length(new_key);
+    int str2Length = length(str2);
+
+    for (i = 0; i < m; i++)
     {
-        if(str[i][j]==hold[0])
+        j = 0;
+        while (str[i][j] != '\0')
         {
-              n=0;
-            for(int k=j;k<j+4;k++)
+            if (str[i][j] == new_key[0])
             {
-                if(str[i][k]==hold[n])
+                n = 0;
+                //for (k = j; k < j + new_keyLength; k++)
                 {
-                    n++;
-                    while(hold[n]=='\0')
+                    if (str
+                        [i][k] == new_key[n])
                     {
-                        printf("LINE IS %s\n",str[i]);
-
-                       int a=i;
-                        break;
-                    }
-
+                        n++;
+                        /*if (n == new_keyLength) //replacing
+                        {
+                            for (int l = 0; l < new_keyLength; l++)
+                            {
+                                str[i][j + l] = str2[l];
+                            }
+                            break;
+                        }
+                    }*/
                 }
             }
+            j++;
         }
-        j++;
     }
 
-   }
-
-
-printf("enter the string to be replaced\n");
-gets(str2);
-for(int i=0;i<10;i++)
-   { j=0;
-    while(str[i][j]!='\0')
+    printf("new strings or paragrahp\n");
+    for (i = 0; i < m; i++)
     {
-        if(str[i][j]==hold[0])
-        {
-              n=0;
-            for(int k=j;k<j+4;k++)
-            {
-                if(str[i][k]==hold[n])
-                {
-                    str[i][k]=str2[n];
-                    n++;
-                    while(hold[n]=='\0')
-                    {
-                        printf("LINE IS %s\n",str[i]);
-                       int a=i;
-
-                        break;
-                    }
-
-                }
-            }
-        }
-        j++;
+        puts(str[i]);
     }
-   }
 }
